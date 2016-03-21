93169d5203514f3d697857c3f2d1fb8231c91220
Hu Songtao <idisblueflash@gmail.com>
2016-03-21 14:58:19 +0800

    fix bug
    ➜  sandworms git:(master) ✗ grunt uglify
    Warning: Task "uglify" not found. Use --force to continue.
    
    Aborted due to warnings.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,7 @@ module.exports = function (grunt) {
       }
     }
   });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-less');
