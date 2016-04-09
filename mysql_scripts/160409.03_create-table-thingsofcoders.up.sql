faae75b13684ee021c928429264301fc7dcae0ce
Hu Songtao <idisblueflash@gmail.com>
2016-04-09 20:19:37 +0800

    fix: wrong type

--- mysql_scripts/160409.03_create-table-thingsofcoders.up.sql
+++ mysql_scripts/160409.03_create-table-thingsofcoders.up.sql
@@ -4,5 +4,5 @@ CREATE TABLE `sandworms`.`things_of_coders` (
   `thing_id` INT NULL,
   `status` VARCHAR(10) NULL COMMENT 'wip, doing, done',
   `work_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT '由于任务会切分成当日可以完成的粒度，所以这里只记录领取的时间。',
-  `close_time` TIMESTAMP,
+  `close_time` DATETIME,
   PRIMARY KEY (`id`));
