1fb6d77e2c3d6ab48ab5ba87ee6f2b6e99eb6f90
Hu Songtao <idisblueflash@gmail.com>
2016-04-20 05:21:53 +0800

    fix: wrong filename

---
+++ mysql_scripts/160420.01_modify-table-thingsofcoders.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `sandworms`.`things_of_coders` 
+CHANGE COLUMN `id` `id` INT(11) NOT NULL ;
---
+++ mysql_scripts/160420.01_modify-table-thingsofcoders.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `sandworms`.`things_of_coders` 
+CHANGE COLUMN `id` `id` INT(11) NOT NULL AUTO_INCREMENT ;
+
