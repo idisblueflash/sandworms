b571c6018197392a654ae692fcea43361f0c93d0
Hu Songtao <idisblueflash@gmail.com>
2016-04-20 06:53:22 +0800

    move order of field

---
+++ mysql_scripts/160420.05_modify-table-thingsofcoders.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `sandworms`.`things_of_coders` 
+CHANGE COLUMN `coder_id` `coder_id` VARCHAR(255) NULL DEFAULT NULL AFTER `id`;
---
+++ mysql_scripts/160420.05_modify-table-thingsofcoders.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `sandworms`.`things_of_coders` 
+CHANGE COLUMN `description` `description` VARCHAR(255) NULL DEFAULT NULL AFTER `id`;
